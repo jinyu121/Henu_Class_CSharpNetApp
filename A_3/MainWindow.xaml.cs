166bb81bf9412b14c7da01f485c329ef6296d4a5
Yu Hao <jinyu121@126.com>
2014-11-11 09:07:41 +0800

    Update A_3

--- A_3/MainWindow.xaml.cs
+++ A_3/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -39,6 +40,8 @@ namespace WpfApplication1
                 try
                 {
                     pro p = new pro(int.Parse(str[0]), int.Parse(str[1]), int.Parse(str[2]), resultShower);
+                    // 注意：在这里使用Parallel.Invoke() 会导致界面死掉。原因未知。
+                    // Parallel.Invoke(() => { p.process(); });
                     Task t = new Task(() => { p.process(); });
                     t.Start();
                 }
@@ -49,12 +52,17 @@ namespace WpfApplication1
         private void ipt_Go_Click(object sender, RoutedEventArgs e)
         {
             ipt.Items.Add(ipt_A.Text + " " + ipt_B.Text + " " + ipt_C.Text);
+            ipt_A.Text = "";
+            ipt_B.Text = "";
+            ipt_C.Text = "";
         }
     }
     public class pro
     {
         int a, b, c;
         ListBox lv;
+        // 这里使用并行选项
+        ParallelOptions op;
         public pro(int a, int b, int c, ListBox lv)
         {
             this.a = a;
@@ -69,19 +77,27 @@ namespace WpfApplication1
             int[,] z = new int[a, c];
             Stopwatch iWatch = new Stopwatch();
             // 初始化
-            Parallel.For(0, a, (i) =>
+            // 这里使用了“按范围分区”
+            OrderablePartitioner<Tuple<int, int>> range;
+            range = Partitioner.Create(0, a);
+            Parallel.ForEach(range, (r, loopstate) =>
             {
-                for (int j = 0; j < b; j++)
-                    x[i, j] = i + j;
+                for (int i = r.Item1; i < r.Item2; i++)
+                    for (int j = 0; j < b; j++)
+                        x[i, j] = i + j;
             });
-            Parallel.For(0, b, (i) =>
+            range = Partitioner.Create(0, b);
+            Parallel.ForEach(range, (r, loopstate) =>
             {
-                for (int j = 0; j < c; j++)
-                    y[i, j] = i + j;
+                for (int i = r.Item1; i < r.Item2; i++)
+                    for (int j = 0; j < c; j++)
+                        y[i, j] = i + j;
             });
+            op = new ParallelOptions();
+            op.MaxDegreeOfParallelism = -1;
             // 开始算
             iWatch.Start();
-            Parallel.For(0, b, (k) =>
+            Parallel.For(0, b, op, (k) =>
             {
                 for (int i = 0; i < a; i++)
                     for (int j = 0; j < c; j++)
@@ -91,7 +107,14 @@ namespace WpfApplication1
             // 完成了
             lv.Dispatcher.Invoke(() =>
             {
-                lv.Items.Add(String.Format("计算用时：{0}毫秒：\r\n\t数组1大小：{1}*{2}\r\n\t数组2大小：{2}*{3}\r\n\t数组3大小：{1}*{3}", iWatch.ElapsedMilliseconds, a, b, c));
+                StringBuilder ss=new StringBuilder();
+                ss.AppendLine(String.Format("计算用时：{0}毫秒：", iWatch.ElapsedMilliseconds));
+                ss.AppendLine(String.Format("\t数组 1 大小：{0}*{1}",a,b));
+                ss.AppendLine(String.Format("\t数组 2 大小：{0}*{1}", b, c));
+                ss.AppendLine(String.Format("\t数组 3 大小：{0}*{1}", a, c));
+                ss.AppendLine(String.Format("\t最大并行度：{0}", op.MaxDegreeOfParallelism));
+                ss.AppendLine();
+                lv.Items.Add(ss.ToString());
             });
         }
     }
